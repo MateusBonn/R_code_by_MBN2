f63970ec8d2f8082a5e6d7d76b475ab16b89db15
Mateus Bonn Ferreira <97714093+MateusBonn@users.noreply.github.com>
2022-03-07 01:07:10 -0300

    s

---
+++ Atividade prática em R.R
@@ -0,0 +1,9 @@
+nome<-c("Marcos de Aguiar","Paula Miranda","Emilio Santos","Priscila de Albuquerque")
+#nmax é opcional, serve apenas para dizer a quantidade de informações
+## o "character" serve para informar o tipo de dados que ficará armazenada 
+n<-scan(what ="character",nmax=4)
+nome[4]<-"Pryscilla de Albuquerque"
+nome
+salario
+salario<-0.7*salario
+salario
--- vetores em R.R
+++ vetores em R.R
@@ -36,10 +36,70 @@ vendas[4]<-70
 vendas[5:7]<- c(23,43,15)
 vendas
 
-## Operações com vetores
+## Operações de vetores com variáveis
 
 length(vendas)
 vendas + 2
 vendas * 2
 vendas / 2
 vendas - 2
+vendas ^ 2
+
+A <- log(vendas)
+log(vendas)
+mean(vendas)
+max(vendas)
+sqrt(vendas)
+
+##crescente
+sort(vendas)
+
+## Decrescente
+sort(vendas, decreasing = T)
+A
+
+## OPERAÇÕES ENTRE VETORES 
+#(é obrigatorio que os vetores tenham o mesmo tamnho)
+
+vendas3 <- c(1,2,3,4,5,6,7)
+vendas1<- c(7,6,5,4,3,2,1)
+
+vendas3 + vendas1
+vendas3 - vendas1
+vendas3 * vendas1
+vendas3 / vendas1
+vendas3 ^ vendas1
+
+v1 <- c(1,2,3,4,5,6)
+v2 <- c(1,2,3,4,5,7)
+
+union(v1,v2)
+intersect(v1,v2)
+setdiff(v1,v2)
+setdiff(v2,v1)
+
+v3 <- interaction(v1,v2)
+expand.grid(v1,v2)
+v3
+length(v3)
+
+va <- c(1,2,3,10)
+vb <- c(4,5,6)
+vc <- c(va,vb)
+vc
+
+## Vetores lógicos
+
+v1 > 3
+v1 < 3 
+v1 == 3
+v1 != 3
+
+sum(v1 != 1)
+v5 <- c(1,1,1,2,2,2,3,3,3)
+sum(v1,v2)
+sum(v5 != 1)
+
+match(2, v5)
+
+va <- scan(nmax = 2) ## Diz a quantidade de dados de entrada no vetor
