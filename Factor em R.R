d9f856c8c972a9bd5936ccb051380dad42b7a973
Mateus Bonn Ferreira <97714093+MateusBonn@users.noreply.github.com>
2022-03-11 17:57:54 -0300

    Bases de R

--- Factor em R.R
+++ Factor em R.R
@@ -1,17 +1,60 @@
 ## Fatores ##
-# Usado para categorizar dados  com indice de identifica��o#
+# Usado para categorizar dados  com indice de identifica<U+FFFD><U+FFFD>o#
 
 # Criando um factor a base de um vetor
 
-genero <- c("1","2","2","1","2","1","1","3","3")
+genero <- c("1", "2", "2", "1", "2", "1", "1", "3", "3")
 genero_ft <- as.factor(genero)
 
 str(genero)
 str(genero_ft)
- 
+
 # ou #
 
 # Criando um factor diretamente
 
-Fator <- factor(c("1","2","2","1","2","1","1","3","3"))
+Fator <- factor(c("1", "2", "2", "1", "2", "1", "1", "3", "3"))
 str(Fator)
+
+# Acessando e modificando fatores
+
+genero_ft[5] # acessa o indice
+length(genero_ft)
+genero_ft[length(genero_ft)]
+
+# Modificando o fator
+genero_ft[5] <- 3
+
+# Operações com fatores
+Produtos_Online <- factor(c("Tv A", "Tv B", "Tv B", "Tv C"))
+Produtos_presencial <- factor(c("Tv A","Tv C", "Tv D", "Tv C","Tv C","Tv D"))
+
+library(forcats)
+
+Vendas <- fct_c(Produtos_Online,Produtos_presencial)
+
+ # ALTERANDO O LABEL
+
+plot(genero_ft)
+
+Alterado <- lvls_revalue(genero_ft, c("Masculino", "Feminino", "Outros")) # Não altera a variável original apenas os labels (todos)
+
+plot(Alterado)
+
+levels(genero_ft)<-c("Masculino", "Feminino", "Outros") # Altera o valor da variável
+
+
+label_especifico <-fct_recode(genero_ft,Masculino="1") # Altera um label especifico ainda preservando a variável original
+
+plot(label_especifico)
+
+# Agrupando de niveis
+
+local <- factor(c("net", "internet", "Instagram","Facebook", "Amigo"))
+Indica <- fct_collapse(local, Pesquisa=c("net", "internet"),
+rede_social=c("Instagram","Facebook"),
+Indicação=c("Amigo"))
+
+plot(Indica)
+
+# Agrupamento por frequencia usando fct_lump
