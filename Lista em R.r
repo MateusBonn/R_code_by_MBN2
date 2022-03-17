701c83763c9228cbf70cb5396baf29cd5960817e
Mateus Bonn Ferreira <97714093+MateusBonn@users.noreply.github.com>
2022-03-17 13:24:53 -0300

    Bases de R

---
+++ Lista em R.r
@@ -0,0 +1,16 @@
+## Lista ##
+# Junção de diversos tipos de dados em um unico local
+
+# Criação de duas formas
+
+clientes_nomes <- c("Mateus", "Marcos", "Maria", "Sophia")
+quantidade <- c(12,10,20,12)
+ID_clientes <- seq(1,4)
+novo_cliente <- c(TRUE, FALSE, FALSE, TRUE)
+
+balan�o <- list(ID_clientes, clientes_nomes, quantidade, novo_cliente, matrix(0,4,5), mean)
+
+####
+list(ID=seq(1,4), nome=c("marcos" ,"tereza", "roberto", "maria"), quant=c(12,10,22,12), new_client=c(TRUE,FALSE,FALSE,TRUE))
+
+     
